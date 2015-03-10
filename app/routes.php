840f6b43e01ae8c2938562dfe623f03ed6cf50ad
MarquesDev <marques.developpement@gmail.com>
2015-03-10 22:31:58 +0100

    Commit

--- app/routes.php
+++ app/routes.php
@@ -4,36 +4,4 @@ $route->get('/', 'HomeController@index')->name('home');
 
 $route->get('/logout', 'HomeController@logout');
 
-$route->get('/create', 'ManageController@createView')->name('create');
-
 $route->post('/', 'HomeController@login');
-
-/**
- * JSON
- */
-$app->get('/api/markers', function() use ($app){
-    $model = new Marker();
-    $map = $model->getAll();
-    $map = json_encode($map);
-
-    $app->response->headers->set('Content-Type', 'application/json');
-    echo $map;
-});
-
-$app->get('/api/markers/:id', function($id) use ($app){
-    $model = new Marker();
-    $map = $model->getOne($id);
-    $map = json_encode($map);
-
-    $app->response->headers->set('Content-Type', 'application/json');
-    echo $map;
-});
-
-$app->get('/api/wars', function() use ($app){
-    $model = new Wars();
-    $map = $model->getAll();
-    $map = json_encode($map);
-
-    $app->response->headers->set('Content-Type', 'application/json');
-    echo $map;
-});
