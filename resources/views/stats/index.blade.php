cbcc27c3b8698c7129ce507a1ffa92431e358dbc
TanQuanHo <tanquanho@gmail.com>
2015-05-04 22:39:57 +0200

    Fait: "Staitistiques" controller + view
    A faire :
    integrer la page dans le menu

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -1,5 +1,42 @@
 @extends('app')
 
 @section('content')
-    {{ $posts }}
+
+    <div class="container-fluid">
+        <div class="col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    POSTS > [<span class="text-info"> Total: {{ $posts }} </span>]
+                </div>
+                <div class="panel-body">
+                    <span class="col-sm-12"><span class="text-info">publiés: </span>{{ $posts_published }}</span>
+                    <span class="col-sm-12"><span class="text-info">stickés: </span>{{ $posts_sticky }}</span>
+                    <span class="col-sm-12"><span class="text-info">en cours d'édition: </span>{{ $posts_editing }}</span>
+                    <span class="col-sm-12"><span class="text-info">... : </span>infos</span>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    USERS > [<span class="text-info"> Total: {{ $users }} </span>]
+                </div>
+                <div class="panel-body">
+                    <span class="col-sm-12"><span class="text-info">élèves: </span>{{ $users }}</span>
+                    <span class="col-sm-12"><span class="text-info">abonnés: </span>{{ $users }}</span>
+                    <span class="col-sm-12"><span class="text-info">... : </span>infos</span>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    OVERVIEW:[<span class="text-info"> Ici : une liste de staitistiques synthétisant l'activité du site </span>]
+                </div>
+                <div class="panel-body">
+                    <span class="col-sm-12"><span class="text-info">Nombre de questions: </span>{{ $questions }}</span>
+                    <span class="col-sm-12"><span class="text-info">... : </span>infos</span>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
+
+
+
