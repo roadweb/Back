17f959ae9a9386b335eddd80cce2dd46f850e555
celine24 <peyrot.celine@gmail.com>
2015-06-12 13:39:44 +0200

    Ajout des fichiers pour le developpement de la partie "derniers articles" de l'index
    transfert des fichiers PHP + SCSS de la branche develop vers la branche
    upstream develop

---
+++ resources/views/index_last_articles.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DERNIERS ARTICLES INDEX</title>
+<link href="../../public/css/sass/index_last_articles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<section class="rw-index-last-articles-container">
+
+	<h1>Derniers articles du blog</h1>
+
+	<article class="rw-index-last-article">
+		<h1>Titre de l'article</h1>
+		<img src="" alt="">
+		<p>description de l'article</p>
+	</article>
+	<article class="rw-index-last-article">
+		<h1>Titre de l'article</h1>
+		<img src="" alt="">
+		<p>description de l'article</p>
+	</article>
+	<article class="rw-index-last-article">
+		<h1>Titre de l'article</h1>
+		<img src="" alt="">
+		<p>description de l'article</p>
+	</article>
+
+</section>
+
+<section class="rw-index-open-articles-container">
+	<article class="rw-index-open-article">
+		<h1>Tutoriels</h1>
+		<div class="rw-index-open-article-tuto">
+			<img src="" alt="">
+			<div class="rw-index-open-article-date">
+				<p><font class="rw-index-day">12</font><br>
+			Janvier<br> 2015</p>
+		</div>
+			<p>titre de l'article</p>
+		</div>
+	</article>
+	<article class="rw-index-open-article">
+		<h1>Codothèque</h1>
+		<div class="rw-index-open-article-tuto">
+			<img src="" alt="">
+			<div class="rw-index-open-article-date">
+				<p><font class="rw-index-day">12</font><br>
+			Janvier<br> 2015</p>
+		</div>
+			<p>titre de l'article</p>
+		</div>
+	</article>
+	<article class="rw-index-open-article">
+		<h1>Outils</h1>
+		<div class="rw-index-open-article-tuto">
+			<img src="" alt="">
+			<div class="rw-index-open-article-date">
+				<p><font class="rw-index-day">12</font><br>
+			Janvier <br>2015</p>
+		</div>
+			<p>titre de l'article</p>
+		</div>
+	</article>
+</section>
+
+<section class="rw-index-open-articles-container-min">
+	<article class="rw-index-open-article-min">
+		<div class="rw-index-article-min-date">
+			<p><font class="rw-index-day">12</font><br>
+			Janvier <br>2015</p>
+		</div>
+		<div class="rw-index-article-min-link">
+			<a href="#">Nom de l'article - lire l'article</a>
+		</div>
+</article>
+	<article class="rw-index-open-article-min">
+		<div class="rw-index-article-min-date">
+			<p><font class="rw-index-day">12</font><br>
+			Janvier <br>2015</p>
+		</div>
+		<div class="rw-index-article-min-link">
+			<a href="#">Nom de l'article - lire l'article</a>
+		</div>
+	</article>
+	<article class="rw-index-open-article-min">
+		<div class="rw-index-article-min-date">
+			<p><font class="rw-index-day">12</font><br>
+			Janvier <br>2015</p>
+		</div>
+		<div class="rw-index-article-min-link">
+			<a href="#">Nom de l'article - lire l'article</a>
+		</div>
+	</article>
+</section>
+
+</body>
+</html>
+
+
