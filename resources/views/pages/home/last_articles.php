b245acc0f61764329c2a4f6c16d568be5f2d58d5
celine24 <peyrot.celine@gmail.com>
2015-06-12 18:35:04 +0200

    INDEX - blocs derniers articles terminé
    html + scss finis, retouches + ménage à voir

--- resources/views/pages/home/last_articles.php
+++ resources/views/pages/home/last_articles.php
@@ -14,17 +14,17 @@
 
 	<article class="rw-index-last-article">
 		<h1>Titre de l'article</h1>
-		<img src="" alt="">
+		<img src="../../../../public/images/last_article_test.jpg" alt="">
 		<p>description de l'article</p>
 	</article>
 	<article class="rw-index-last-article">
 		<h1>Titre de l'article</h1>
-		<img src="" alt="">
+		<img src="../../../../public/images/last_article_test.jpg" alt="">
 		<p>description de l'article</p>
 	</article>
 	<article class="rw-index-last-article">
 		<h1>Titre de l'article</h1>
-		<img src="" alt="">
+		<img src="../../../../public/images/last_article_test.jpg" alt="">
 		<p>description de l'article</p>
 	</article>
 
@@ -34,7 +34,7 @@
 	<article class="rw-index-open-article">
 		<h1>Tutoriels</h1>
 		<div class="rw-index-open-article-tuto">
-			<img src="" alt="">
+			<img src="../../../../public/images/open_article_test.jpg" alt="">
 			<div class="rw-index-open-article-date">
 				<p><font class="rw-index-day">12</font><br>
 			Janvier<br> 2015</p>
@@ -44,8 +44,8 @@
 	</article>
 	<article class="rw-index-open-article">
 		<h1>Codothèque</h1>
-		<div class="rw-index-open-article-tuto">
-			<img src="" alt="">
+		<div class="rw-index-open-article-codo">
+			<img src="../../../../public/images/open_article_test.jpg" alt="">
 			<div class="rw-index-open-article-date">
 				<p><font class="rw-index-day">12</font><br>
 			Janvier<br> 2015</p>
@@ -55,12 +55,12 @@
 	</article>
 	<article class="rw-index-open-article">
 		<h1>Outils</h1>
-		<div class="rw-index-open-article-tuto">
-			<img src="" alt="">
+		<div class="rw-index-open-article-outi">
 			<div class="rw-index-open-article-date">
 				<p><font class="rw-index-day">12</font><br>
 			Janvier <br>2015</p>
-		</div>
+			</div>
+			<img src="../../../../public/images/open_article_test.jpg" alt="">
 			<p>titre de l'article</p>
 		</div>
 	</article>
@@ -74,6 +74,7 @@
 		</div>
 		<div class="rw-index-article-min-link">
 			<a href="#">Nom de l'article - lire l'article</a>
+			<img src="../../../../public/images/metier_test.png" alt="">
 		</div>
 </article>
 	<article class="rw-index-open-article-min">
@@ -82,7 +83,8 @@
 			Janvier <br>2015</p>
 		</div>
 		<div class="rw-index-article-min-link">
-			<a href="#">Nom de l'article - lire l'article</a>
+			<a href="#">Nom de l'article - lire l'article blablatest</a>
+			<img src="../../../../public/images/metier_test.png" alt="">
 		</div>
 	</article>
 	<article class="rw-index-open-article-min">
@@ -92,6 +94,7 @@
 		</div>
 		<div class="rw-index-article-min-link">
 			<a href="#">Nom de l'article - lire l'article</a>
+			<img src="../../../../public/images/metier_test.png" alt="">
 		</div>
 	</article>
 </section>
