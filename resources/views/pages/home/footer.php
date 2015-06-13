07ab4a38c729840fdadf72f72e981c97431db733
celine24 <peyrot.celine@gmail.com>
2015-06-13 14:55:19 +0200

    Ajout des fichiers footer

---
+++ resources/views/pages/home/footer.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DERNIERS ARTICLES INDEX</title>
+<link href="../../../../public/css/sass/footer.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<footer>
+
+
+</footer>
+
+</body>
+</html>
