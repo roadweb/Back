4b5054393373afe65410ed2e3b473f5fa1322d63
celine24 <peyrot.celine@gmail.com>
2015-06-13 21:09:21 +0200

    footer terminé
    retouches à faire + ajout du bcg à réception du fichier img

--- resources/views/pages/home/footer.php
+++ resources/views/pages/home/footer.php
@@ -9,13 +9,24 @@
 <body>
 
 <footer>
-<div class="rw-index-contact"></div>
-<div class="rw-index-btn-subscription"></div>
+<div class="rw-index-footer-contact"><p>Pour écrire à l'équipe de Roadweb, remplissez le <a href="">formulaire</a> du site. Nous vous répondrons le plus rapidement possible.</p></div>
+<div class="rw-index-footer-btn-subscription"><a href="#">Abonnement à Roadweb</a></div>
+<div class="clear"></div>
 <div class="rw-index-footer-nav">
-	<img src="" alt="">
-	<p>Nos réseaux sociaux</p>
+	<div class="rw-index-footer-test"></div><!-- quand l'image sera disponible <img src="" alt="">-->
+	<p>Nos Réseaux Sociaux</p>
 	<ul>
-		<li></li>
+		<li><a href=""><img src="../../../../public/images/linkedin.png" alt=""></a></li>
+		<li><a href=""><img src="../../../../public/images/twitter.png" alt=""></a></li>
+		<li><a href=""><img src="../../../../public/images/facebook.png" alt=""></a></li>
+		<li><a href=""><img src="../../../../public/images/pinterest.png" alt=""></a></li>
+		<li><a href=""><img src="../../../../public/images/google-plus.png" alt=""></a></li>
+		<li><a href=""><img src="../../../../public/images/behance.png" alt=""></a></li>
+	</ul>
+	<ul>
+		<li><a href="">Mentions légales</a></li>
+		<li><a href="">Contact</a></li>
+		<li><a href="">Abonnement</a></li>
 	</ul>
 </div>
 
