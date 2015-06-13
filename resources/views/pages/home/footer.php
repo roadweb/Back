28d0c54574061a791af03901108bf9c87c0a8191
celine24 <peyrot.celine@gmail.com>
2015-06-13 16:50:25 +0200

    Ajout des images dans le dossier

--- resources/views/pages/home/footer.php
+++ resources/views/pages/home/footer.php
@@ -2,14 +2,22 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>DERNIERS ARTICLES INDEX</title>
+<title>FOOTER INDEX</title>
 <link href="../../../../public/css/sass/footer.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
 <footer>
-
+<div class="rw-index-contact"></div>
+<div class="rw-index-btn-subscription"></div>
+<div class="rw-index-footer-nav">
+	<img src="" alt="">
+	<p>Nos réseaux sociaux</p>
+	<ul>
+		<li></li>
+	</ul>
+</div>
 
 </footer>
 
