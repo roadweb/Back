b88325ce83c35ebe83c5899e3e954afa513df85b
titzelle <pseudoups@hotmail.fr>
2015-06-14 23:21:20 +0200

    Header et bannière Html / scss
    J'ai mis un body à 1000 px et le reste en pourcentage.
    Le rouge de l'index.jpg ne correspond pas au rouge du code hexadecimal donné dans le guide line. C'est normal ?

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,7 +1,35 @@
 @extends('pages.app')
 
 @section('content')
+    <header class="rw-header_container">
+        <div class="rw-header_top">
+            <p>
+                <img class="rw-logo" alt="logo hexagonal roadweb" src="{{asset('images/logo.png')}}">
+            </p>
 
-    <p>Je suis la home</p>
+            <p class="rw-slogan">bienvenue sur <em>roadweb</em>.com</p>
 
+            <div class="rw-search">
+                <form action="" class="rw-searchbar" method="post">
+                    <input name="" type="search" placeholder="Rechercher sur Roadweb.com"/>
+                </form>
+                <p><img class="rw-loupe" alt="loupe" src="{{asset('images/loupe.png')}}" height="90px"></p>
+            </div>
+        </div>
+        <div class="rw-clear"></div>
+        <nav class="rw-nav">
+            <ul class="rw-ul_grey">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Forum</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Tutoriel</a></li>
+                <li><a href="#">Exercices</a></li>
+                <li><a href="#">Codothèque</a></li>
+            </ul>
+            <ul class="rw-ul_red">
+                <li><a href="#">Cvthèque</a></li>
+                <li><a href="#">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
 @stop
