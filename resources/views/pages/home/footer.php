74701fdb702af0f4586c1ee1a71beca8d58d38a1
celine24 <peyrot.celine@gmail.com>
2015-06-16 11:21:41 +0200

    Ajout du background hexagones dans le footer

--- resources/views/pages/home/footer.php
+++ resources/views/pages/home/footer.php
@@ -9,26 +9,32 @@
 <body>
 
 <footer>
-<div class="rw-index-footer-contact"><p>Pour écrire à l'équipe de Roadweb, remplissez le <a href="#">formulaire</a> du site. Nous vous répondrons le plus rapidement possible.</p></div>
-<div class="rw-index-footer-btn-subscription"><a href="#">Abonnement à Roadweb</a></div>
-<div class="clear"></div>
-<div class="rw-index-footer-nav">
-	<div class="rw-index-footer-nav-bcg-icon"></div>
-	<p>Nos Réseaux Sociaux</p>
-	<ul>
-		<li><a href=""><img src="../../../../public/images/linkedin.png" alt="linkedin"></a></li>
-		<li><a href=""><img src="../../../../public/images/twitter.png" alt="twitter"></a></li>
-		<li><a href=""><img src="../../../../public/images/facebook.png" alt="facebook"></a></li>
-		<li><a href=""><img src="../../../../public/images/pinterest.png" alt="pinterest"></a></li>
-		<li><a href=""><img src="../../../../public/images/google-plus.png" alt="google +"></a></li>
-		<li><a href=""><img src="../../../../public/images/behance.png" alt="behance"></a></li>
-	</ul>
-	<ul>
-		<li><a href="">Mentions légales</a></li>
-		<li><a href="">Contact</a></li>
-		<li><a href="">Abonnement</a></li>
-	</ul>
-</div>
+
+	<img class="rw-index-footer-bcg" src="../../../../public/images/fond-exa-footer.png">
+
+	<div class="rw-index-footer-container">
+		<div class="rw-index-footer-contact"><p>Pour écrire à l'équipe de Roadweb, remplissez le <a href="#">formulaire</a> du site. Nous vous répondrons le plus rapidement possible.</p></div>
+		<div class="rw-index-footer-btn-subscription"><a href="#">Abonnement à Roadweb</a></div>
+		<div class="clear"></div>
+		<div class="rw-index-footer-nav">
+		<div class="rw-index-footer-nav-bcg-icon"></div>
+			<p>Nos Réseaux Sociaux</p>
+			<ul>
+				<li><a href=""><img src="../../../../public/images/linkedin.png" alt="linkedin"></a></li>
+				<li><a href=""><img src="../../../../public/images/twitter.png" alt="twitter"></a></li>
+				<li><a href=""><img src="../../../../public/images/facebook.png" alt="facebook"></a></li>
+				<li><a href=""><img src="../../../../public/images/pinterest.png" alt="pinterest"></a></li>
+				<li><a href=""><img src="../../../../public/images/google-plus.png" alt="google +"></a></li>
+				<li><a href=""><img src="../../../../public/images/behance.png" alt="behance"></a></li>
+			</ul>
+			<ul>
+				<li><a href="">Mentions légales</a></li>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Abonnement</a></li>
+			</ul>
+		</div>
+	</div>
+
 </footer>
 
 </body>
