2637eae6e760a041fe6b23bed9dffb227ac9ddba
TanQuanHo <tanquanho@gmail.com>
2015-06-23 13:41:48 +0200

    carrousel
    Développement en statique;
    modifications dans : "_base.scss" ; "_reset.scss" ; "_variables" ;

---
+++ resources/views/pages/home/carrousel/carrousel.blade.php
@@ -0,0 +1,10 @@
+<!-- **********************    SECTION "CAROUSSEL"    *************************** -->
+<!-- **********************    MATHIEU HO             src="images/exagone-top.png"      *************************** -->
+
+    <img class="rw-fond-exagone-top" src="images/exagonefond.png" />
+    <div class="rw-carrous">
+        <div>
+            @include('pages.home.carrousel.component.jobnav')
+            @include('pages.home.carrousel.component.jobdescription')
+        </div>
+    </div>
