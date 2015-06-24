c6aec2f1871f0dc25aa1cd111b35cf3a859ae3a2
MarquesDev <marques.developpement@gmail.com>
2015-06-24 22:26:42 +0200

    bug

--- resources/views/pages/components/header.blade.php
+++ resources/views/pages/components/header.blade.php
@@ -4,11 +4,17 @@
             <img class="rw-logo" alt="logo hexagonal roadweb" src="{{asset('images/logo.png')}}">
         </p>
 
+<<<<<<< HEAD
         <h1 class="rw-slogan">bienvenue sur <em>roadweb</em>.com</h1>
 
         <div id="rw-search">
             {{--ajoute la classe transSearch quand il y a un scroll--}}
 
+=======
+        <p class="rw-slogan">bienvenue sur <em>roadweb</em>.com</p>
+
+        <div class="rw-search">
+>>>>>>> 6b26a5423a3baeba6d1edc886b567ca91b9d32ca
             <form action="" class="rw-searchbar" method="post">
                 <input name="" type="search" placeholder="Rechercher sur Roadweb.com"/>
             </form>
@@ -16,6 +22,7 @@
         </div>
     </div>
     <div class="rw-clear"></div>
+<<<<<<< HEAD
     <nav id="rw-nav" >
         {{--ajoute la classe fixNavigation quand il y a un scroll--}}
         <ul class="rw-ul_grey" >
@@ -25,6 +32,20 @@
         </ul>
         <ul class="rw-ul_red">
             <li><a href="#">Abonnement</a></li>
+=======
+    <nav class="rw-nav">
+        <ul class="rw-ul_grey">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Tutoriel</a></li>
+            <li><a href="#">Exercices</a></li>
+            <li><a href="#">Codothèque</a></li>
+        </ul>
+        <ul class="rw-ul_red">
+            <li><a href="#">Cvthèque</a></li>
+            <li><a href="#">Connexion</a></li>
+>>>>>>> 6b26a5423a3baeba6d1edc886b567ca91b9d32ca
         </ul>
     </nav>
 </header>
