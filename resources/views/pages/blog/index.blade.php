74b7b7195f8b910c7d21a14a1e797d9c6de69863
celine24 <peyrot.celine@gmail.com>
2015-07-10 11:35:48 +0200

    conflit resolu

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -6,7 +6,6 @@
 
     <div class="container">
 
-    	@include('pages.carrousel.component.jobnav')
         @include('pages.components.last-articles')
         @include('pages.components.last-articles-min')
 
