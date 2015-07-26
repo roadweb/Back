55c46c6d18256da6c79dfdc5f9610147f7bc96ac
celine24 <peyrot.celine@gmail.com>
2015-07-26 20:12:50 +0200

    test cle etrangere supprime

---
+++ database/migrations/2015_04_25_154847_create_jobs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Job;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jobs', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name');
+			$table->timestamps();
+            $table->string('resume');
+            $table->longText('content');
+            $table->string('title');
+            $table->string('job_icone');
+            $table->string('job_color_clair');
+            $table->string('job_color_fonce');
+
+		});
+
+        Job::create(['name' => 'Developpeur']);
+        Job::create(['name' => 'Designer']);
+        Job::create(['name' => 'Community Manager']);
+        Job::create(['name' => 'Web Marketer']);
+        Job::create(['name' => 'Integrateur']);
+        Job::create(['name' => 'Referenceur']);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobs');
+	}
+
+}
