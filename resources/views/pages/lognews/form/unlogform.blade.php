332105e2bd8b49b7e22c5b2c648504d89865d13a
--global <pseudoups@hotmail.fr>
2015-07-31 01:11:52 +0200

    modification css mathieu

--- resources/views/pages/lognews/form/unlogform.blade.php
+++ resources/views/pages/lognews/form/unlogform.blade.php
@@ -24,7 +24,8 @@
             <div class="rw-lognews-icon">
                 <img src="{{ asset('images/icons-manquantes/ok.png') }}" alt="ok"/>
             </div>
-            <a href="{{ URL::to('/auth/logout') }}">Se déconnecter</a>
+            <button type="submit"><a href="{{ URL::to('/auth/logout') }}">Se déconnecter</a></button>
+            {{--<a href="{{ URL::to('/auth/logout') }}">Se déconnecter</a>--}}
         </div>
     </div>
     
