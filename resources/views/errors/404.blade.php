a53278e726da99953ae70282a8ba73152d369291
celine24 <peyrot.celine@gmail.com>
2015-08-16 12:33:11 +0200

    gestion des erreurs, vues + traduction

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('pages.app')
+
+@section('content')
+
+<div class="container">
+    <div class="rw-error-404-container">
+
+    	<p>Désolé, la page que vous avez demandé n'<span class="rw-error-red">existe pas</span> !</p>
+
+	</div>
+</div>
+
+@stop
