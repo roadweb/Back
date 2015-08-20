9ceea190de00bdc46545c349cc298403d487b259
thepopix <paulinelateulere@yahoo.fr>
2015-08-20 12:52:06 +0200

    reglage liens pour kcfinder

--- public/unisharp/ckeditor/config.js
+++ public/unisharp/ckeditor/config.js
@@ -7,11 +7,11 @@ CKEDITOR.editorConfig = function( config ) {
         // Define changes to default configuration here. For example:
         // config.language = 'fr';
 	    // config.uiColor = '#AADC6E';
-    config.filebrowserBrowseUrl = '../../kcfinder/browse.php?opener=ckeditor&type=files';
-    config.filebrowserImageBrowseUrl = '../../kcfinder/browse.php?opener=ckeditor&type=images';
-    config.filebrowserFlashBrowseUrl = '../../kcfinder/browse.php?opener=ckeditor&type=flash';
-    config.filebrowserUploadUrl = '../../kcfinder/upload.php?opener=ckeditor&type=files';
-    config.filebrowserImageUploadUrl = '../../kcfinder/upload.php?opener=ckeditor&type=images';
-    config.filebrowserFlashUploadUrl = '../../kcfinder/upload.php?opener=ckeditor&type=flash';
+    config.filebrowserBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=files';
+    config.filebrowserImageBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=images';
+    config.filebrowserFlashBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=flash';
+    config.filebrowserUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=files';
+    config.filebrowserImageUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=images';
+    config.filebrowserFlashUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=flash';
 
 };
