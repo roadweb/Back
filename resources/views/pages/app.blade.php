b5b4deed8d88b1d2b9e0cabe7b6934d5e912b0b7
thepopix <paulinelateulere@yahoo.fr>
2015-09-11 23:09:30 +0200

    modifs article.blade pour techno et meta FB et twitter

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -5,6 +5,20 @@
     <meta name="viewport" content="width=device-width, user-scalable=yes" />
     <title>Roadweb</title>
     <meta name="description" content="Road-Web est un site communautaire rassemblant des élèves et des professionnels juniors dans les métiers du web : fiches métiers, articles autour du web…">
+   {{--meta facebook--}}
+    <meta property="og:title" content="Road Web" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="Road-Web est un site communautaire rassemblant des élèves et des professionnels juniors dans les métiers du web : fiches métiers, articles autour du web…" />
+    <meta property="og:url" content="http://road-web.fr" />
+    <meta property="og:image" content="http://road-web.fr/images/logo.png" />
+
+    {{--meta twitter--}}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@roadweb" />
+    <meta name="twitter:title" content="RoadWeb" />
+    <meta name="twitter:description" content="Road-Web est un site communautaire rassemblant des élèves et des professionnels juniors dans les métiers du web : fiches métiers, articles autour du web…" />
+    <meta name="twitter:image" content="http://road-web.fr/images/logo.png" />
+
     <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}"/>
     <link href="{{ asset('bower_components/dw-bxslider-4/dist/jquery.bxslider.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/front/style.css') }}"/>
